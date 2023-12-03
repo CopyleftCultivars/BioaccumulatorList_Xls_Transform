7716d0585472513da8f4f9a23b4157f3316e2474
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-12-02 17:21:54 -0800

    Add files via upload

--- BioAccumulatorXlsPageMerger.py
+++ BioAccumulatorXlsPageMerger.py
@@ -20,6 +20,7 @@ for sheet_name, df in xls.items():
     # Remove the first two rows
     df = df.iloc[2:]
     df.columns = df.iloc[0]
+    df = df[1:]
 
     print(f'Sheet name: {sheet_name}')
     print(f'Columns: {df.columns.tolist()}')
@@ -29,35 +30,16 @@ for sheet_name, df in xls.items():
         df = df.rename(columns={'Min': f'Min. {sheet_name}', 'Max': f'Max. {sheet_name}'})
     
     df.columns = list(rename_duplicates(df.columns))  # Rename duplicate columns
+    df = df.reset_index(drop=True)  # Reset the index
     dfs.append(df)
 
-# Concatenate all DataFrames
-combined_df = pd.concat(dfs)
+# Concatenate all DataFrames vertically
+combined_df = pd.concat(dfs, ignore_index=True)
 
-# Define a function to apply for each column
-def agg_func(x):
-    if x.dtypes == 'float64' and x.notnull().all():
-        return x.mean()
-    else:
-        return x.first()
-
-# Combine rows with the same scientific or latin name
-combined_df = combined_df.groupby(['Scientific Name', 'Latin Name']).apply(agg_func)
-
-# Remove rows with no data
-combined_df = combined_df.dropna(how='all')
-
-# Remove rows containing the specific string
-string_to_remove = "Questions, Comments, Additions, New Sources of Data? Email us:  OpenNutrientProject@gmail.com          Compiled by Originally by Stephen Raisner : PotentPonics@gmail.com. "
-mask = combined_df.applymap(lambda cell: string_to_remove in str(cell)).any(axis=1)
-combined_df = combined_df[~mask]
-
-# Reset the index without adding it as a column
-combined_df.reset_index(drop=True, inplace=True)
-
-# Merge rows with duplicated text in the 'Scientific Name' and 'Latin Name' columns
-combined_df = combined_df.groupby(['Scientific Name', 'Latin Name'], as_index=False).first()
-
-# Save the combined DataFrame to a new Excel file
-output_file = 'C:/Users/caleb/Downloads/Nutrient_Bioaccumulator_combined_data.xlsx'
-combined_df.to_excel(output_file, index=False)
+# If combined_df is still empty after concatenation, print an error message
+if combined_df.empty:
+    print("Error: The combined DataFrame is empty. Please check the data in the Excel file.")
+else:
+    # Save the combined DataFrame to a new Excel file
+    output_file = 'C:/Users/caleb/Downloads/Nutrient_Bioaccumulator_combined_data.xlsx'
+    combined_df.to_excel(output_file, index=False)

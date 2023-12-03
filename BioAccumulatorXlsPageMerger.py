8447df1534c879846863bd61b91804cd8cbfdff9
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-12-02 16:21:50 -0800

    Add files via upload

--- BioAccumulatorXlsPageMerger.py
+++ BioAccumulatorXlsPageMerger.py
@@ -1,19 +1,39 @@
 import pandas as pd
 
-xls_file = r'C:\Users\caleb\Downloads\Nutrient Bio Accumulators.xlsx'
-xls = pd.read_excel(xls_file, sheet_name=None)
+xls_file = r'C:\Users\caleb\Downloads\Copy of Nutrient Bio Accumulators.xlsx'
+xls = pd.read_excel(xls_file, sheet_name=None, header=None)
 
 dfs = []  # list to store modified DataFrames
 
+def rename_duplicates(old):
+    seen = {}
+    for x in old:
+        if x in seen:
+            seen[x] += 1
+            yield "%s.%d" % (x, seen[x])
+        else:
+            seen[x] = 0
+            yield x
+
 # Iterate over each sheet
 for sheet_name, df in xls.items():
+    # Remove the first two rows
+    df = df.iloc[2:]
+    df.columns = df.iloc[0]
+    df = df[1:]
+
     print(f'Sheet name: {sheet_name}')
     print(f'Columns: {df.columns.tolist()}')
 
     # Rename 'Min' and 'Max' columns
     if 'Min' in df.columns and 'Max' in df.columns:
         df = df.rename(columns={'Min': f'Min. {sheet_name}', 'Max': f'Max. {sheet_name}'})
-        dfs.append(df)
+    
+    df.columns = list(rename_duplicates(df.columns))  # Rename duplicate columns
+    df = df.reset_index(drop=True)  # Reset the index
+    dfs.append(df)
+
+# Rest of your code...
 
 # Check if dfs is empty
 if not dfs:

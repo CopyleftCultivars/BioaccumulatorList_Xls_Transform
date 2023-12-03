e89afb36b30aaceef65d585c85162393f9cf6f60
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-12-02 16:38:52 -0800

    Add files via upload

--- BioAccumulatorXlsPageMerger.py
+++ BioAccumulatorXlsPageMerger.py
@@ -20,7 +20,6 @@ for sheet_name, df in xls.items():
     # Remove the first two rows
     df = df.iloc[2:]
     df.columns = df.iloc[0]
-    df = df[1:]
 
     print(f'Sheet name: {sheet_name}')
     print(f'Columns: {df.columns.tolist()}')
@@ -30,25 +29,34 @@ for sheet_name, df in xls.items():
         df = df.rename(columns={'Min': f'Min. {sheet_name}', 'Max': f'Max. {sheet_name}'})
     
     df.columns = list(rename_duplicates(df.columns))  # Rename duplicate columns
-    df = df.reset_index(drop=True)  # Reset the index
     dfs.append(df)
 
-# Rest of your code...
+# Concatenate all DataFrames
+combined_df = pd.concat(dfs)
 
-# Check if dfs is empty
-if not dfs:
-    print('No DataFrames to concat.')
-else:
-    # Concat. all DataFrames
-    combined_df = pd.concat(dfs)
+# Define a function to apply for each column
+def agg_func(x):
+    if x.dtypes == 'float64' and x.notnull().all():
+        return x.mean()
+    else:
+        return x.first()
 
-    # Remove rows containing the specific string
-    string_to_remove = "Questions, Comments, Additions, New Sources of Data? Email us:  OpenNutrientProject@gmail.com          Compiled by Originally by Stephen Raisner : PotentPonics@gmail.com. "
-    mask = combined_df.applymap(lambda cell: string_to_remove in str(cell)).any(axis=1)
-    combined_df = combined_df[~mask]
+# Combine rows with the same scientific or latin name
+combined_df = combined_df.groupby(['Scientific Name', 'Latin Name']).apply(agg_func)
+
+# Remove rows with no data
+combined_df = combined_df.dropna(how='all')
+
+# Remove rows containing the specific string
+string_to_remove = "Questions, Comments, Additions, New Sources of Data? Email us:  OpenNutrientProject@gmail.com          Compiled by Originally by Stephen Raisner : PotentPonics@gmail.com. "
+mask = combined_df.applymap(lambda cell: string_to_remove in str(cell)).any(axis=1)
+combined_df = combined_df[~mask]
+
+# Reset the index without adding it as a column
+combined_df.reset_index(drop=True, inplace=True)
 
 # Merge rows with duplicated text in the 'Scientific Name' and 'Latin Name' columns
-combined_df = combined_df.groupby(['Scientific Name', 'Latin Name']).first().reset_index()
+combined_df = combined_df.groupby(['Scientific Name', 'Latin Name'], as_index=False).first()
 
 # Save the combined DataFrame to a new Excel file
 output_file = 'C:/Users/caleb/Downloads/Nutrient_Bioaccumulator_combined_data.xlsx'
